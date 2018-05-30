1fb649146de9c3ceaa8a187b42339ce28b43a3ba
Karun Japhet <karun@japhet.in>
2018-05-30 00:29:09 -0400

    Move migrations to it's own dedicated file

--- src/main/scala/me/karun/migration/graph/Application.scala
+++ src/main/scala/me/karun/migration/graph/Application.scala
@@ -4,29 +4,15 @@ import me.karun.migration.graph.MigrationYamlProtocol._
 import me.karun.migration.graph.model.Migration
 import net.jcazevedo.moultingyaml._
 
+import scala.io.Source
+
 object Application extends App {
   println("Starting migration") // TODO: Migrate all println statements to slf4j
 
-  // TODO: Read from dedicated yaml file
-  val yaml =
-    """
-      |-
-      |  version: "1.0"
-      |  author: karun
-      |  query: "CREATE (user:Users {name:'Karun',last_name:'AB',tokens:3})"
-      |  rollback: "MATCH (u:Users) WHERE u.name = 'Karun' DELETE u"
-      |-
-      |  version: "2.0"
-      |  author: karun
-      |  query: "CREATE (user:Users {name:'Mansi',last_name:'Vartak',tokens:6})"
-      |  rollback: "MATCH (u:Users) WHERE u.name = 'Mansi' DELETE u"
-      |-
-      |  version: "3.0"
-      |  author: karun
-      |  query: "CREATE (user:Users {name:'Bim',last_name:'Japhet',tokens:6})"
-      |  rollback: "MATCH (u:Users) WHERE u.name = 'Bim' DELETE u"
-    """.stripMargin.parseYaml
-  val migrationPlan = yaml.convertTo[List[Migration]]
+  val migrationPlan = Source.fromFile("src/main/resources/migrations.yaml")
+    .mkString
+    .parseYaml
+    .convertTo[List[Migration]]
 
   val status = migrationPlan
     .filter(_.isExecutable)
